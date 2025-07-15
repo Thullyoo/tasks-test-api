4852818d764126f2e9ef9853ae72905136971619
Thullyoo <thullyofilho@gmail.com>
2025-07-15 12:14:42 -0300

    feat: update code due change of dependencies

--- src/test/java/br/thullyoo/api_test/APITest.java
+++ src/test/java/br/thullyoo/api_test/APITest.java
@@ -1,53 +1,52 @@
 package br.thullyoo.api_test;
 
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 
 public class APITest {
 
-    @BeforeEach
-    public void setup(){
+    @BeforeClass
+    public static void setup() {
         RestAssured.baseURI = "http://localhost:8001/tasks-backend";
     }
 
     @Test
-    public void shouldReturnTasks(){
+    public void shouldReturnTasks() {
         RestAssured
                 .given()
                 .when()
-                    .get("todo")
+                .get("todo")
                 .then()
-                    .log().all()
-                    .statusCode(200)
-
-        ;
+                .statusCode(200);
     }
 
     @Test
-    public void shouldSaveTaskWithSuccess(){
+    public void shouldSaveTaskWithSuccess() {
         RestAssured
                 .given()
-                    .body("{\"task\": \"task1\", \"dueDate\": \"2030-12-10\"}")
-                    .contentType(ContentType.JSON)
+                .body("{\"task\": \"task1\", \"dueDate\": \"2030-12-10\"}")
+                .contentType(ContentType.JSON)
                 .when()
-                    .post("todo")
+                .post("todo")
                 .then()
-                    .statusCode(201);
+                .statusCode(201);
     }
 
     @Test
-    public void shouldntSaveInvalidTask(){
+    public void shouldntSaveInvalidTask() {
         RestAssured
                 .given()
-                    .body("{\"task\": \"task1\", \"dueDate\": \"2010-12-10\"}")
-                    .contentType(ContentType.JSON)
+                .body("{\"task\": \"task1\", \"dueDate\": \"2010-12-10\"}")
+                .contentType(ContentType.JSON)
                 .when()
-                    .post("todo")
+                .post("todo")
                 .then()
-                    .statusCode(400)
-                    .body("message", CoreMatchers.is("Due date must not be in past"));
+                .statusCode(400)
+                .body("message", CoreMatchers.is("Due date must not be in past"));
     }
 }
